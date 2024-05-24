e85dc96e5b56bc45d76c3241dc25cfae5757eb0f
Loki690 <nobodym920@gmail.com>
2024-05-24 16:33:32 +0800

    added point-of-sale and order-list

---
+++ custom_app/customapp/page/order_list/order_list.js
@@ -0,0 +1,21 @@
+frappe.provide("erpnext.PointOfSale");
+
+frappe.pages["order-list"].on_page_load = function (wrapper) {
+	frappe.ui.make_app_page({
+		parent: wrapper,
+		title: __("Order List"),
+		single_column: true,
+	});
+	frappe.require("order-list.bundle.js", function () {
+		wrapper.pos = new erpnext.PointOfSale.Controller(wrapper);
+		window.cur_pos = wrapper.pos;
+	});
+};
+
+frappe.pages["order-list"].refresh = function (wrapper) {
+	if (document.scannerDetectionData) {
+		onScan.detachFrom(document);
+		wrapper.pos.wrapper.html("");
+		// wrapper.pos.check_opening_entry();
+	}
+};
