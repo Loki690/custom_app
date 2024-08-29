e450a559b5ecff82bd07da91daa4d223c6a719f9
Loki690 <nobodym920@gmail.com>
2024-08-29 08:42:59 +0800

    feat: csd report

---
+++ custom_app/customapp/doctype/csd_daily_sales_report_detailed/csd_daily_sales_report_detailed.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, joncsr and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("CSD Daily Sales Report Detailed", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
