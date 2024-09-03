15292cce93ce708bee880127e6fe8fdd212c4906
Loki690 <nobodym920@gmail.com>
2024-09-03 14:04:27 +0800

    chore: transfer principal

--- custom_app/public/dist/js/amesco-point-of-sale.bundle.PDRQ3AA3.js
+++ custom_app/public/dist/js/amesco-point-of-sale.bundle.PDRQ3AA3.js
@@ -6237,6 +6237,7 @@
           in_list_view: 1,
           label: "Mode of Payment",
           options: "Mode of Payment",
+          read_only: 1,
           reqd: 1
         },
         {
@@ -6244,6 +6245,7 @@
           fieldtype: "Currency",
           in_list_view: 1,
           label: "Opening Amount",
+          read_only: 1,
           options: "company:company_currency",
           change: function() {
             dialog2.fields_dict.balance_details.df.data.some((d) => {
@@ -6318,7 +6320,8 @@
             in_place_edit: true,
             reqd: 1,
             data: [],
-            fields: table_fields
+            fields: table_fields,
+            read_only: 1
           }
         ],
         primary_action: async function({ company, pos_profile, balance_details }) {
@@ -6471,6 +6474,43 @@
       });
       passwordDialog.show();
     }
+    showSystemManagerPasswordDialog(title, onSuccess) {
+      const passwordDialog = new frappe.ui.Dialog({
+        title: __(title),
+        fields: [
+          {
+            fieldname: "password",
+            fieldtype: "Password",
+            label: __("Password"),
+            reqd: 1
+          }
+        ],
+        primary_action_label: __("Authorize"),
+        primary_action: (values) => {
+          let password = values.password;
+          frappe.call({
+            method: "custom_app.customapp.page.amesco_point_of_sale.amesco_point_of_sale.confirm_system_manager_password",
+            args: { password },
+            callback: (r) => {
+              if (r.message.name) {
+                frappe.show_alert({
+                  message: __("Verified"),
+                  indicator: "green"
+                });
+                passwordDialog.hide();
+                onSuccess();
+              } else {
+                frappe.show_alert({
+                  message: __("Incorrect password or user is not an OIC"),
+                  indicator: "red"
+                });
+              }
+            }
+          });
+        }
+      });
+      passwordDialog.show();
+    }
     z_reading() {
       const onSuccess = () => {
         if (!this.$components_wrapper.is(":visible"))
@@ -6486,7 +6526,7 @@
           frappe.msgprint(__("Failed to fetch POS Profile. Please try again."));
         });
       };
-      this.showPasswordDialog("OIC Authorization Required for Z Reading", onSuccess);
+      this.showSystemManagerPasswordDialog("System Manager Authorization Required for Z Reading", onSuccess);
     }
     dsrs_reading() {
       const onSuccess = () => {
@@ -7448,4 +7488,4 @@
     }
   };
 })();
-//# sourceMappingURL=amesco-point-of-sale.bundle.FD2REFKP.js.map
+//# sourceMappingURL=amesco-point-of-sale.bundle.PDRQ3AA3.js.map
